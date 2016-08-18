0fa9c52dfeb262c1b52c179ac52cb03755e12f95
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-08-18 08:55:25 -0700

    Use glide to load image thumbnails.

--- app/src/main/java/com/greysonparrelli/mynews/adapters/FeedItemAdapter.java
+++ app/src/main/java/com/greysonparrelli/mynews/adapters/FeedItemAdapter.java
@@ -4,11 +4,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.greysonparrelli.mynews.R;
 import com.greysonparrelli.mynews.fragments.FeedFragment;
 import com.greysonparrelli.mynews.models.FeedItem;
+import com.greysonparrelli.mynews.utils.HtmlUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,6 +57,7 @@ public class FeedItemAdapter extends RecyclerView.Adapter<FeedItemAdapter.FeedIt
 
     public static class FeedItemViewHolder extends RecyclerView.ViewHolder {
 
+        private ImageView mThumbnail;
         private TextView mTitle;
         private TextView mLink;
         private FeedFragment.FeedController mController;
@@ -62,6 +66,7 @@ public class FeedItemAdapter extends RecyclerView.Adapter<FeedItemAdapter.FeedIt
             super(itemView);
             mController = controller;
 
+            mThumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
             mTitle = (TextView) itemView.findViewById(R.id.title);
             mLink = (TextView) itemView.findViewById(R.id.link);
         }
@@ -76,6 +81,11 @@ public class FeedItemAdapter extends RecyclerView.Adapter<FeedItemAdapter.FeedIt
                     mController.showFeedItem(feedItem);
                 }
             });
+
+            List<String> imageUrls = HtmlUtil.getImgUrls(feedItem.getContent());
+            if (imageUrls.size() > 0) {
+                Glide.with(itemView.getContext()).load(imageUrls.get(0)).into(mThumbnail);`
+            }
         }
 
         public void recycle() {
