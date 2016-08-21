ae5c9305d14dd6ebfcd0a430a47db292d3af0ce7
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-08-21 01:49:14 -0700

    Added pull-to-refresh.

--- app/src/main/java/com/greysonparrelli/mynews/fragments/FeedFragment.java
+++ app/src/main/java/com/greysonparrelli/mynews/fragments/FeedFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -24,10 +25,11 @@ import okhttp3.Call;
 /**
  * @author Greyson Parrelli (keybase.io/greyson)
  */
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     public static String TAG = "FeedListFragment";
 
+    private SwipeRefreshLayout mSwipeLayout;
     private RecyclerView mList;
     private FeedItemAdapter mAdapter;
     private Feed mFeed;
@@ -56,6 +58,10 @@ public class FeedFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mSwipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh);
+        mSwipeLayout.setOnRefreshListener(this);
+
         mList = (RecyclerView) view.findViewById(R.id.list);
         mList.setLayoutManager(new LinearLayoutManager(getContext()));
     }
@@ -66,16 +72,24 @@ public class FeedFragment extends Fragment {
         mAdapter = new FeedItemAdapter((FeedController) getActivity());
         mList.setAdapter(mAdapter);
 
+        // Load data
+        onRefresh();
+    }
+
+    @Override
+    public void onRefresh() {
         NetworkUtil.getFeed("http://www.theverge.com/rss/index.xml", new NetworkUtil.FeedCallback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 Toast.makeText(getContext(), "FAILED", Toast.LENGTH_SHORT).show();
+                mSwipeLayout.setRefreshing(false);
             }
 
             @Override
             public void onResponse(Feed feed) {
                 mFeed = feed;
                 mAdapter.setFeedItems(mFeed.getEntries());
+                mSwipeLayout.setRefreshing(false);
             }
         });
     }
